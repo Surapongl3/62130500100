0737389f6617fc50ebc0b967e4e2d228e04abcb1
Surapong Chamalai <surapong.bird@mail.kmutt.ac.th>
2021-01-27 10:20:44 +0700

    First commit

---
+++ main.js
@@ -0,0 +1,27 @@
+// const app = {​​​​
+
+//     data() {​​​​
+
+//         return {​​​​
+
+//             msg: 'Hello, Vue3'
+
+//         }​​​​
+
+//     }​​​​
+
+
+
+// }​​​​
+
+// Vue.createApp(app).mount('#app')
+const app ={
+
+    data(){
+
+        return {
+            msg: 'Hello,Vue3'
+        }
+    }
+} 
+    Vue.createApp(app).mount('#app')
