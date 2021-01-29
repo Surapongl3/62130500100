95c9265298583be900cc205f860566dc20d421c3
Surapong Chamalai <surapong.bird@mail.kmutt.ac.th>
2021-01-29 21:25:00 +0700

    navbar+Searchbar

--- main.js
+++ main.js
@@ -1,20 +1,4 @@
-// const app = {​​​​
 
-//     data() {​​​​
-
-//         return {​​​​
-
-//             msg: 'Hello, Vue3'
-
-//         }​​​​
-
-//     }​​​​
-
-
-
-// }​​​​
-
-// Vue.createApp(app).mount('#app')
 const app ={
 
     data(){
